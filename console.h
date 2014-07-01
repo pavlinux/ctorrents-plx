845549718d5cd1346c608998d5bf68fca25684fd
pavlinux <pavlinux@bingo-boom.ru>
2014-07-02 02:57:03 +0400

    Console header hacking

--- console.h
+++ console.h
@@ -1,15 +1,8 @@
 #include "def.h"
-#include <sys/types.h>  // fd_set
 #include <stdarg.h>
 #include <stdio.h>
-
-#if defined(USE_TERMIOS)
 #include <termios.h>
-#elif defined(USE_TERMIO)
-#include <termio.h>
-#elif defined(USE_SGTTY)
-#include <sgtty.h> 
-#endif
+#include <sys/types.h>  // fd_set
 
 #include "rate.h"
 
@@ -24,118 +17,138 @@
 #define O_DEBUG     3
 #define O_INPUT     4  // not an output!  do not include in above count.
 
+class ConStream {
+private:
+    FILE *m_stream;
+    char *m_name;
+    unsigned char m_newline : 1;
+    unsigned char m_suspend : 1;
+    unsigned char m_inputmode : 1;
+    unsigned char m_filemode : 1;
+    unsigned char m_restore : 1;
+    unsigned char m_reserved : 3;
 
-class ConStream
-{
- private:
-  FILE *m_stream;
-  char *m_name;
-  unsigned char m_newline:1;
-  unsigned char m_suspend:1;
-  unsigned char m_inputmode:1;
-  unsigned char m_filemode:1;
-  unsigned char m_restore:1;
-  unsigned char m_reserved:3;
+    struct termios m_original;
+    void _newline();
+    int _convprintf(const char *format, va_list ap);
+    void Error(int sev, const char *message, ...);
 
-#if defined(USE_TERMIOS)
-  struct termios m_original;
-#elif defined(USE_TERMIO)
-  struct termio m_original;
-#elif defined(USE_SGTTY)
-  struct sgttyb m_original;
-#endif
+public:
+    ConStream();
+    ~ConStream();
 
-  void _newline();
-  int _convprintf(const char *format, va_list ap);
-  void Error(int sev, const char *message, ...);
+    void Close();
+    void Associate(FILE *stream, const char *name, int mode);
 
- public:
-  ConStream();
-  ~ConStream();
+    char *GetName() const {
+        return m_name;
+    }
 
-  void Close();
-  void Associate(FILE *stream, const char *name, int mode);
-  char *GetName() const { return m_name; }
-  int GetMode() const { return m_filemode ? 1 : 0; }
-  int Fileno() const { return m_stream ? fileno(m_stream) : -1; }
-  int GetNewline() const { return m_newline ? 1 : 0; }
-  void SyncNewline(ConStream *master) { m_newline = master->GetNewline(); }
-  void Suspend() { m_suspend = 1; }
-  void Resume() { m_suspend = 0; }
-  int IsSuspended() { return m_suspend ? 1 : 0; }
+    int GetMode() const {
+        return m_filemode ? 1 : 0;
+    }
 
-  int SameDev(ConStream *master) const;
-  int GetInputMode() const { return m_inputmode; }
-  void SetInputMode(int keymode);
-  void PreserveMode();
-  void RestoreMode();
-  int IsTTY() const;
+    int Fileno() const {
+        return m_stream ? fileno(m_stream) : -1;
+    }
 
-  int Output(const char *message, va_list ap);
-  int Output_n(const char *message, va_list ap);
-  int Update(const char *message, va_list ap);
-  char *Input(char *field, size_t length);
-  int CharIn();
+    int GetNewline() const {
+        return m_newline ? 1 : 0;
+    }
 
-  int Eof() const { return feof(m_stream); }
+    void SyncNewline(ConStream *master) {
+        m_newline = master->GetNewline();
+    }
+
+    void Suspend() {
+        m_suspend = 1;
+    }
+
+    void Resume() {
+        m_suspend = 0;
+    }
+
+    int IsSuspended() {
+        return m_suspend ? 1 : 0;
+    }
+
+    int SameDev(ConStream *master) const;
+
+    int GetInputMode() const {
+        return m_inputmode;
+    }
+    void SetInputMode(int keymode);
+    void PreserveMode();
+    void RestoreMode();
+    int IsTTY() const;
+
+    int Output(const char *message, va_list ap);
+    int Output_n(const char *message, va_list ap);
+    int Update(const char *message, va_list ap);
+    char *Input(char *field, size_t length);
+    int CharIn();
+
+    int Eof() const {
+        return feof(m_stream);
+    }
 };
 
+class Console {
+private:
+    unsigned char m_live_idx : 2;
+    unsigned char m_conmode : 1;
+    unsigned char m_skip_status : 1;
+    unsigned char m_status_last : 1;
+    unsigned char m_reserved : 3;
 
-class Console
-{
- private:
-  unsigned char m_live_idx:2;
-  unsigned char m_conmode:1;
-  unsigned char m_skip_status:1;
-  unsigned char m_status_last:1;
-  unsigned char m_reserved:3;
+    int m_status_format;
+    int m_oldfd;
+    int m_status_len;
 
-  int m_status_format;
-  int m_oldfd;
-  int m_status_len;
+    typedef void (Console::*statuslinefn)(char buffer[], size_t length);
+    statuslinefn m_statusline[STATUSLINES];
 
-  typedef void (Console::*statuslinefn)(char buffer[], size_t length);
-  statuslinefn m_statusline[STATUSLINES];
+    Rate m_pre_dlrate, m_pre_ulrate;
 
-  Rate m_pre_dlrate, m_pre_ulrate;
+    ConStream m_stdout, m_stderr, m_stdin, m_off;
+    ConStream *m_streams[O_NCHANNELS + 1];
 
-  ConStream m_stdout, m_stderr, m_stdin, m_off;
-  ConStream *m_streams[O_NCHANNELS+1];
+    int OpenNull(int nullfd, ConStream *stream, int sfd);
+    void SyncNewlines(int master);
+    int OperatorMenu(const char *param);
+    void ShowFiles();
+    void StatusLine0(char buffer[], size_t length);
+    void StatusLine1(char buffer[], size_t length);
 
-  int OpenNull(int nullfd, ConStream *stream, int sfd);
-  void SyncNewlines(int master);
-  int OperatorMenu(const char *param);
-  void ShowFiles();
-  void StatusLine0(char buffer[], size_t length);
-  void StatusLine1(char buffer[], size_t length);
+public:
+    Console();
+    ~Console();
 
- public:
-  Console();
-  ~Console();
+    int IntervalCheck(fd_set *rfdp, fd_set *wfdp);
+    void User(fd_set *rfdp, fd_set *wfdp, int *nready,
+            fd_set *rfdnextp, fd_set *wfdnextp);
+    void Status(int immediate);
 
-  int IntervalCheck(fd_set *rfdp, fd_set *wfdp);
-  void User(fd_set *rfdp, fd_set *wfdp, int *nready,
-    fd_set *rfdnextp, fd_set *wfdnextp);
-  void Status(int immediate);
+    void Print(const char *message, ...);
+    void Print_n(const char *message, ...);
+    void Update(const char *message, ...);
+    void Warning(int sev, const char *message, ...);
+    void Debug(const char *message, ...);
+    void Debug_n(const char *message, ...);
+    void Interact(const char *message, ...);
+    void Interact_n(const char *message, ...);
+    void InteractU(const char *message, ...);
+    char *Input(const char *prompt, char *field, size_t length);
 
-  void Print(const char *message, ...);
-  void Print_n(const char *message, ...);
-  void Update(const char *message, ...);
-  void Warning(int sev, const char *message, ...);
-  void Debug(const char *message, ...);
-  void Debug_n(const char *message, ...);
-  void Interact(const char *message, ...);
-  void Interact_n(const char *message, ...);
-  void InteractU(const char *message, ...);
-  char *Input(const char *prompt, char *field, size_t length);
+    char *GetChannel(int channel) const {
+        return m_streams[channel]->GetName();
+    }
+    int ChangeChannel(int channel, const char *param, int notify = 1);
 
-  char *GetChannel(int channel) const { return m_streams[channel]->GetName(); }
-  int ChangeChannel(int channel, const char *param, int notify = 1);
+    void cpu();
 
-  void cpu();
-
-  RETSIGTYPE Signal(int sig_no);
-  void Daemonize();
+    RETSIGTYPE Signal(int sig_no);
+    void Daemonize();
 };
 
 extern Console CONSOLE;
